37ab42bdc17e252134e6ac652120d8d817e0dee0
khristinatsymbala <khristinakorda@gmail.com>
2024-04-05 16:55:02 +0300

    SFML window

--- Learn C++ and Make a Game from Scratch/LightYears/LightYears/src/main.cpp
+++ Learn C++ and Make a Game from Scratch/LightYears/LightYears/src/main.cpp
@@ -1,7 +1,23 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
-
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+    sf::RenderWindow renderWindow(sf::VideoMode(200, 200), "SFML works!");
+    
+
+    while (renderWindow.isOpen())
+    {
+        sf::Event windowEvent;
+        while (renderWindow.pollEvent(windowEvent))
+        {
+            if (windowEvent.type == sf::Event::Closed)
+                renderWindow.close();
+        }
+
+        renderWindow.clear();
+        renderWindow.display();
+    }
+
+    return 0;
 }
