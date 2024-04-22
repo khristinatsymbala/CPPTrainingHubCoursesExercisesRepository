1885d3960f29da795c3950ba3c7c00ae05710157
khristinatsymbala <khristinakorda@gmail.com>
2024-04-22 19:25:25 +0300

    print subarrays

---
+++ Data Structures & Algorithms Essentials using C++/01_Arrays/07_print_subarrays.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Subarray is small part of array
+// Problem : Print all subarrays of a given array
+void printSubarrays(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                std::cout << arr[k] << ",";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
+
+int main()
+{
+    int array[] = {10, 20, 30, 40, 56, 78, 89};
+    int n = sizeof(array) / sizeof(int);
+
+    printSubarrays(array, n);
+
+    return 0;
+}
