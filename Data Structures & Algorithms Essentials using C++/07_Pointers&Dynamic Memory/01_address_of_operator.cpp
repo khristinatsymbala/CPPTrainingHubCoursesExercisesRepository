ba1f081d6b9244665833f36fafdf5c49a7d3616d
khristinatsymbala <khristinakorda@gmail.com>
2024-05-02 15:46:34 +0300

    Dereferencing operator

--- Data Structures & Algorithms Essentials using C++/07_Pointers&Dynamic Memory/01_address_of_operator.cpp
+++ Data Structures & Algorithms Essentials using C++/07_Pointers&Dynamic Memory/01_address_of_operator.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //Address of operator - & 
-
+//&bucket => address
 
 int main(){
 
--- Data Structures & Algorithms Essentials using C++/07_Pointers&Dynamic Memory/02_dererefence_operator.cpp
+++ Data Structures & Algorithms Essentials using C++/07_Pointers&Dynamic Memory/02_dererefence_operator.cpp
@@ -1,18 +1,34 @@
 #include<iostream>
+#include <string>
 using namespace std;
+//Dereferencing operator *
+
+/* An interesting property of pointers is tha they can be used to access the variable they point to directly.
+This is done by preceding the pointer name with the dereferencing operator(*).
+The operator itself can be read as "value pointed to by"
+*/
+
+//*address => bucket(data)
 
 int main(){
 
-	int x = 10;
-	int &y = x;
+	string food = "Pizza";  // Variable declaration
+	string* ptr = &food;    // Pointer declaration
 
-	y++; 
+	// Reference: Output the memory address of food with the pointer (0x6dfed4)
+	cout << ptr << "\n";
 
-	cout << x <<endl;
-	cout << y <<endl;
+	// Dereference: Output the value of food with the pointer (Pizza)
+	cout << *ptr << "\n";
 
 
+	// int x = 10;
+	// int &y = x;
 
+	// y++; 
+
+	// cout << x <<endl;
+	// cout << y <<endl;
 
 	return 0;
 }
